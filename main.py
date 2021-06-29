227b64b70fb6b6a46467a71c52faccba3616fcd7
Elsayed H Elsory <elsayed.7amada@gmail.com>
2021-06-29 21:45:18 +0200

    Create the statement for close the Game

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-start = input("Do you Want To Play Or Not (y/n) >>").lower()
-
-# Welcome Message
 print("Welcome To Quizey Game With Mr Jack😉.")
+start = input("Do you Want To Play Or Not (y/n) >>").lower()
+score = 0
+if start != "y":
+    print("See You Later😎.")
+    quit()
+# Welcome Message
 print("Note>> if you answer right you get 1 score and if wrong you lose one")
 print("Note>> This Game is 7 questions")
-score = 0
+
