aa68e264519ba50d3d510555c20d320aede2f012
Elsayed H Elsory <elsayed.7amada@gmail.com>
2021-06-29 22:11:12 +0200

    Write Function To Check The Answer✔

--- main.py
+++ main.py
@@ -7,4 +7,22 @@ if start != "y":
 # Welcome Message
 print("Note>> if you answer right you get 1 score and if wrong you lose one")
 print("Note>> This Game is 7 questions")
+print("Let's Start With the First Question.")
+
+
+def answer_checker(q, a, s):
+    if q == a:
+        print("You Got it.")
+        s += 1
+        print(f"Your Score is {s}")
+    else:
+        if s == 0:
+            print("sorry, Wrong")
+            s = 0
+            print(f"Your Score is {s}")
+        else:
+            print("sorry, Wrong")
+            s -= 1
+            print(f"Your Score is {s}")
+    print(f"Answer >> {a}")
 
