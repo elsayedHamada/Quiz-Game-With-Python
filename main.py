7e8be53c07769296b5d959bdacf244bd671d2f83
Elsayed H Elsory <elsayed.7amada@gmail.com>
2021-06-29 21:39:53 +0200

    Write The Welcome Messages🙌.

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
+start = input("Do you Want To Play Or Not (y/n) >>").lower()
+
 # Welcome Message
+print("Welcome To Quizey Game With Mr Jack😉.")
+print("Note>> if you answer right you get 1 score and if wrong you lose one")
+print("Note>> This Game is 7 questions")
+score = 0
